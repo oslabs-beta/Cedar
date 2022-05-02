ccdfacb345a10af126edca67b07c5c3c27728144
Carly Yarnell <carly.yarnell21@gmail.com>
2022-05-02 16:46:54 -0400

    class to functional comps

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class Logs extends Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <h1>ello</h1>
-    )
-  }
+const Logs = () => {
+  return(
+    <h1>ello</h1>
+  )
 }
+
+// class Logs extends Component {
+//   constructor(props){
+//     super(props);
+//   }
+
+//   render(){
+//     return(
+//       <h1>ello</h1>
+//     )
+//   }
+// }
 
 export default Logs;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,57 +1,46 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import { TextField, Grid, Paper, Button, AppBar } from '@mui/material';
 import Lobby from './Lobby';
 import Home from './Home';
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      create: false,
-      cancel: false
-    }
-    this.createAcct = this.createAcct.bind(this)
-    this.cancelSignup = this.cancelSignup.bind(this)
+const Signup = () => {
+  const [ create, setCreate ] = useState(false);
+  const [ cancel, setCancel ] = useState(false);
+
+  const handleCreateChange = () => {
+    setCreate(true);
   }
 
-  createAcct(){
-    if(this.state.create === false){
-      this.setState({create: true})
-    }
+  const handleCancelChange = () => {
+    setCancel(true);
   }
+  const paperStyle = {padding: 30, height: '45vh auto', width:300, margin: '10px auto', opacity: 0.75};
+  if(create === true){
+    return <Home />
+  }
+  if(cancel === true){
+    return <Lobby />
+  }
+  return(
+    <>
+    <Grid>
+      <Paper align= 'left' elevation= {10} style= {paperStyle}>
+        <Grid align= 'center'>
+          <h3>Sign Up</h3>
+        </Grid>
+        <TextField label='Email' placeholder='Enter email' fullwidth="true" />
+        <h4></h4>
+        <TextField label='Password' placeholder='Enter password' fullwidth="true" />
+        <h4></h4>
+        <Button variant="contained" color= 'secondary' onClick= {handleCreateChange} >Create Account</Button>
+        <Button variant="text"  color= 'secondary' onClick= {handleCancelChange}>Cancel</Button>
+      </Paper>
+    </Grid>
+    </>
+  )
+  //function to handle the submit click, it will be an addUser post request to server probably passed from lobby
 
-  cancelSignup(){
-    if(this.state.cancel === false){
-      this.setState({cancel: true})
-    }
-  }
-  render(){
-    const paperStyle = {padding: 30, height: '45vh auto', width:300, margin: '10px auto', opacity: 0.75}
-    if(this.state.create === true) {
-      return <Home />
-    }
-    if(this.state.cancel === true) {
-      return <Lobby />
-    }
-    
-    return(
-      <>
-      <Grid>
-        <Paper align= 'left' elevation= {10} style= {paperStyle}>
-          <Grid align= 'center'>
-            <h3>Sign Up</h3>
-          </Grid>
-          <TextField label='Email' placeholder='Enter email' fullwidth="true" />
-          <h4></h4>
-          <TextField label='Password' placeholder='Enter password' fullwidth="true" />
-          <h4></h4>
-          <Button variant="contained" color= 'secondary' onClick= {this.createAcct} >Create Account</Button>
-          <Button variant="text"  color= 'secondary' onClick= {this.cancelSignup}>Cancel</Button>
-        </Paper>
-      </Grid>
-      </>
-    )
-  }
+
 }
 
 export default Signup;
