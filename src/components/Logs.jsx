01ed65198dd69cb4cbf0ba7aa6fdd79ceaa20c11
jeidman <jonah.eidman@gmail.com>
2022-05-04 19:15:35 -0400

    successful fetch of log data

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,8 +1,16 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import { useNavigate } from 'react-router';
 import { Button } from '@mui/material';
+import { getLogs } from '../utils/fetchUtils';
 
 const Logs = () => {
+  useEffect(() => {
+    console.log('getting the logs now');
+    const startTime = Math.floor(Date.now() - (1000*60*60*24*7));
+    const func = 'myloop';
+    getLogs(() => {return}, func, startTime);
+  })
+
   const navigate = useNavigate();
   const handleBackClick = useCallback(() => navigate('/home', {replace: true}), [navigate]);
   return(
