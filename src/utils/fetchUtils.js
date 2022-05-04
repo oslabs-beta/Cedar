fe667cb181631b7a914c5ebd7331c757d738abe2
jeidman <jonah.eidman@gmail.com>
2022-05-03 20:15:09 -0400

    successful fetch and console log of metrics

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -2,7 +2,9 @@ import 'regenerator-runtime'
 
 /**
  * Get functions list from aws lambda, and add to state
- * @param {function} setFunctionData - useState hook setter function for functionData state
+ * @async
+ * @function getFuncs
+ * @param {function} setFunctionData - setter function for functionData state
  * @returns {undefined}
  */
 export const getFuncs = async (setFunctionData) => {
@@ -17,6 +19,40 @@ export const getFuncs = async (setFunctionData) => {
         }
       })
     );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * Get requested function metric data from aws lambda
+ * @async
+ * @function getMetricData
+ * @param {function} setFunctionData - setter function for functionData state
+ * @param {array} functions - list of AWS Lambda functions to query for data
+ * @param {array} metrics - list of AWS Lambda metrics to obtain for each function
+ * @param {number} startTime - unix time from which to begin pulling metrics data
+ * @param {number} endTime - unix time until which to pulling metrics data. Defaults to now, rounded down to nearest minute.
+ */
+export const getMetricData = async (setFunctionData, functions, metrics, startTime, endTime) => {
+  if (!endTime) endTime = Math.floor(Date.now() / (1000*60))*(1000*60);
+  try {
+    console.log('fetching data')
+    const data = await fetch('/api/aws/getMetricData', {
+      method: 'POST',
+      body: JSON.stringify({
+        start: startTime,
+        end: endTime,
+        funcs: functions,
+        metrics
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log('got data')
+    const parsedData = await data.json();
+    console.log(parsedData);
   } catch (err) {
     console.log(err);
   }
