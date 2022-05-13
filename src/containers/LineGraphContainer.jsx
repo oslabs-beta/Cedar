86d07d3f275175194debdfdf7868198f11eddce8
jeidman <jonah.eidman@gmail.com>
2022-05-12 20:41:19 -0400

    document all components, move data processing from LineGraphContainer to a utility file, light testing

--- src/containers/LineGraphContainer.jsx
+++ src/containers/LineGraphContainer.jsx
@@ -1,65 +1,32 @@
 import React from "react";
 import { periods, roundingUtil } from '../utils/conversions';
 import LineGraph from '../components/LineGraph';
+import { mapDatatoTimestamps, createDataset, createLineGraphProps, generateTimestamps, prepGraphFromRawData } from "../utils/chartjsUtils";
 
 const COLORS = ['green', 'purple', 'pink', 'orange', 'blue', 'red', 'yellow'];
-let COLORINDEX = 0
 
+/**
+ * LineGraphContainer component.
+ * Prepares AWS Lambda metric data in state for input into chart.js line graph components
+ * Renders container that holds a chart.js line graph for every metric requests by the user. 
+ */
 function LineGraphContainer(props) {
   const { functions, metrics, period, startTime } = props.displayProps;
   // determine the x axis time unit given the period
   const unitName = periods[period].unitName;
   const unitTime = periods[period].unitMs;
+  
   // get first timestamp as the startTime, rounded based on the unit
   const firstTimeStamp = roundingUtil(startTime, periods[period].unitMs);
+  const endTimeStamp = roundingUtil(Date.now(), periods[period].unitMs);
   // generate timestamps for label given the period
-  const timestamps = [];
-  for (let time = firstTimeStamp; time < Date.now(); time += unitTime) {
-    timestamps.push(time);
-  }
-  const dataProps = [];
-  metrics.forEach(metric => {
-    COLORINDEX = 0;
-    const dataProp = {
-      timestamps,
-      unitName,
-      timeMin: firstTimeStamp,
-      metricName: metric,
-      functions: [],
-    }
-    functions.forEach(func => {
-      const dataset = {
-        id: func,
-        label: func,
-        backgroundColor: COLORS[COLORINDEX],
-        borderColor: COLORS[COLORINDEX],
-      };
-      COLORINDEX++
-      const rawData = props.funcData[func].metrics[metric];
-      // data for plot, aligned with timestamps for plot
-      let rawDataPointer = rawData.timestamps.length - 1;
-      const data = timestamps.map((timestamp, i) => {
-        let dataSum = 0;
-        while (
-          Date.parse(rawData.timestamps[rawDataPointer]) >= timestamp &&
-          i < timestamps.length &&
-          Date.parse(rawData.timestamps[rawDataPointer]) < timestamps[i + 1] &&
-          rawDataPointer >= 0
-        ) {
-          dataSum += rawData.values[rawDataPointer];
-          rawDataPointer--;
-        }
-        return dataSum;
-      });
-      dataset.data = data;
-      dataProp.functions.push(dataset);
-    });
-    dataProps.push(dataProp);
-  });
+  const timestamps = generateTimestamps(firstTimeStamp, endTimeStamp, unitTime);
+
+  const dataProps = prepGraphFromRawData(props.funcData, metrics, functions, timestamps, firstTimeStamp, unitName);
+
   const LineGraphs = dataProps.map(dataProp => {
-    return <LineGraph key={dataProp.metricName} dataProp={dataProp} />
+    return <LineGraph key={dataProp.title} dataProp={dataProp} />
   })
-  console.log(LineGraphs)
   return (
     <>
       {LineGraphs}
