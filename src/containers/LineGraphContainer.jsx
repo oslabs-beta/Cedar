5b3dd549e09177d6db35268293bd64ac2f6abfb9
jeidman <jonah.eidman@gmail.com>
2022-05-13 15:33:58 -0400

    finish LineGraphContainer cleanup

--- src/containers/LineGraphContainer.jsx
+++ src/containers/LineGraphContainer.jsx
@@ -3,8 +3,6 @@ import { periods, roundingUtil } from '../utils/conversions';
 import LineGraph from '../components/LineGraph';
 import { generateTimestamps, prepGraphFromRawData } from "../utils/chartjsUtils";
 
-const COLORS = ['green', 'purple', 'pink', 'orange', 'blue', 'red', 'yellow'];
-
 /**
  * LineGraphContainer component.
  * Prepares AWS Lambda metric data in state for input into chart.js line graph components
@@ -12,16 +10,17 @@ const COLORS = ['green', 'purple', 'pink', 'orange', 'blue', 'red', 'yellow'];
  */
 function LineGraphContainer(props) {
   const { functions, metrics, period, startTime } = props.displayProps;
-  // determine the x axis time unit given the period
+
+  // determine the x axis time unit given the selected time period
   const unitName = periods[period].unitName;
   const unitTime = periods[period].unitMs;
 
-  // get first timestamp as the startTime, rounded based on the unit
+  // generate timestamps for the selected time period
   const firstTimeStamp = roundingUtil(startTime, periods[period].unitMs);
   const endTimeStamp = roundingUtil(Date.now(), periods[period].unitMs);
-  // generate timestamps for label given the period
   const timestamps = generateTimestamps(firstTimeStamp, endTimeStamp, unitTime);
 
+  // generate array of props objects to create line graph components
   const dataProps = prepGraphFromRawData(props.funcData, metrics, functions, timestamps, firstTimeStamp, unitName);
 
   const LineGraphs = dataProps.map(dataProp => {
@@ -32,7 +31,6 @@ function LineGraphContainer(props) {
       {LineGraphs}
     </>
   )
-  //todo
 }
 
  export default LineGraphContainer;
