2b41114c2c31e19aff487d8e6676370004336738
mireille.crocco@gmail.com <mireille.crocco@gmail.com>
2022-05-14 18:32:09 -0400

    react test file WIP

--- __test_/react.js
+++ __test_/react.js
@@ -1,3 +1,5 @@
+//https://www.freecodecamp.org/news/react-testing-library-tutorial-javascript-example-code/
+
 //import
 import React from 'React';
 import userEvent from '@testing-library/user-event';
@@ -37,16 +39,24 @@ describe('Unit testing React presentational components', () => {
           beforeAll(() => {
             card = render(<LineGraph {...lGraphProps} />);      
           });
-          //a graph should be displayed as a <div>
-          //getByRole may not be the way to dispaly a div--research
+
+          //a graph is displayed as a <div> in console
+          //getByRole may not be the way to dispaly a div--research: .getByText maybe?
           //writing a test to match what the app is already doing (delete this comment later...)
           //how do you make sure you're only getting things: what tells us it's a chart, can't grab chart
           //graphs drawn w/ JS and HTML, but how does HTML and JS know how to differneciate b/w the 3 graphs
-          test('renders line graph with correct properties', () => {
-            expect(lGraph.getByRole('img')).toBeVisible();
-
-          })
+       
     })
 })
 
+const lGraph = <canvas role="img" height="297" width="594" class="testChart" 
+style="display: block; box-sizing: border-box; height: 297px; width: 594px;"></canvas>
 
+test('renders line graph with correct properties', () => {
+    //lGraph is the <canvas> element
+    expect(lGraph).toHaveClass('testChart'); //lGraph should have a class assigned to testChart
+    expect(lGraph).toHaveAttribute('role', 'img'); //lGraph should have an attribute role assigned to img
+    //unsure if two lines below are necessary:
+    expect(lGraph).toHaveAttribute('role', expect.stringContaining('img')) //checks if attribute has specific value or partial match
+    expect(lGraph).toHaveClass('testChart', {exact: true}) //checks if element has exactly a set of classes
+  })
