d5425723c66694cdccf26d89526dcfd720ad5058
mireille.crocco@gmail.com <mireille.crocco@gmail.com>
2022-05-14 14:26:43 -0400

    set up React Test Lib

---
+++ __test_/react.js
@@ -0,0 +1,56 @@
+//import
+import React from 'React';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+import regeneratorRuntime from 'regenerator-runtime';
+
+//imports 
+import LineGraph from '../src/components/LineGraph';
+import LogsHeader from '../src/components/LogsHeader';
+import LogsHeaderItem from '../src/components/LogsHeaderItem';
+import MessageRow from '../src/components/MessageRow';
+import MessageRowItem from '../src/components/MessageRowItem';
+
+describe('Unit testing React presentational components', () => {
+    describe('LineGraph', () => {
+        let lGraph;
+        const lGraphProps = {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'bottom',
+              },
+              title: {
+                text: 'metricName'
+              },
+            },
+            scales: {
+              x: {
+                type: 'time',
+                time: {
+                  unit: 'unitName',
+                },
+                min: 'timeMin',
+              },
+              y: {
+                min: 0,
+              }
+            },
+            cubicInterpolationMode: 'monotone',
+          };
+          beforeAll(() => {
+            card = render(<LineGraph {...lGraphProps} />);      
+          });
+          //a graph should be displayed as a <div>
+          //getByRole may not be the way to dispaly a div--research
+          //writing a test to match what the app is already doing (delete this comment later...)
+          //how do you make sure you're only getting things: what tells us it's a chart, can't grab chart
+          //graphs drawn w/ JS and HTML, but how does HTML and JS know how to differneciate b/w the 3 graphs
+          test('renders line graph with correct properties', () => {
+            expect(lGraph.getByRole('img')).toBeVisible();
+
+          })
+    })
+})
+
+
