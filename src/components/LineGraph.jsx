752d0d9dfae092261a7fc32748226cece26225d6
jeidman <jonah.eidman@gmail.com>
2022-05-16 13:38:45 -0400

    chart.js styling for line graph in options, including colors, labels, and global settings for points and lines

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -12,6 +12,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { withTheme } from '@emotion/react';
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -35,17 +36,8 @@ function  LineGraph (props) {
   // set options for chart.js LineGraph components
   const options = {
     responsive: true,
+    color: 'white',
     plugins: {
-      legend: {
-        position: 'bottom',
-        title: {
-          display: true,
-          text: 'AWS Lambda Functions',
-          font: {
-            size: 20,
-          },
-        }
-      },
       title: {
         display: true,
         text: title,
@@ -53,26 +45,79 @@ function  LineGraph (props) {
         font: {
           size: 32
         },
+        color: 'white',
+      },
+      legend: {
+        position: 'bottom',
+        title: {
+          display: true,
+          text: 'AWS Lambda Functions',
+          font: {
+            size: 16,
+          },
+        },
+        labels: {
+          usePointStyle: true,
+        },
       },
     },
     scales: {
       x: {
         type: 'time',
+        title: {
+          display: true,
+          text: 'Time',
+          color: 'white',
+          font: {
+            size: 20,
+          },
+        },
         time: {
           unit: xUnit,
         },
         min: xMin,
+        ticks: {
+          color: 'white',
+        },
+        grid: {
+          color: 'white',
+          borderColor: 'white',
+        },
       },
       y: {
+        title: {
+          display: true,
+          text: 'Metrics',
+          color: 'white',
+          font: {
+            size: 20,
+          },
+        },
         min: yMin,
-      }
+        ticks: {
+          color: 'white',
+        },
+        grid: {
+          color: 'white',
+          borderColor: 'white',
+        },
+      },
     },
-    cubicInterpolationMode: 'monotone',
+    elements: {
+      point: {
+        radius: 4,
+      },
+      line: {
+        borderWidth: 2,
+        borderDash: [4,1],
+        cubicInterpolationMode: 'monotone',
+      },
+    },
   };
    
   return (
-    <div className= 'lineChartContainer'>
-      <Line className='testChart'
+    <div className= 'lineGraphContainer'>
+      <Line className='lineGraph'
         options={options}
         data={{
           labels,
